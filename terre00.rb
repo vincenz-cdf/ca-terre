440dcb2df5fd112224937ab680c324935aa35e2a
Vincent <85881442+vincenz-cdf@users.noreply.github.com>
2023-09-25 11:31:43 +0200

    affiche alphabet avec retour ligne

---
+++ terre00.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+alphabet.each do |letter|
+    puts letter + "\n"
+end
